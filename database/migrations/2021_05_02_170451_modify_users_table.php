08b78ebc41e388f833bcbf57235d8901852e3784
hayato <shu_mon_win2@yahoo.co.jp>
2021-05-02 17:12:16 +0900

    カラムをnullableに変更

---
+++ database/migrations/2021_05_02_170451_modify_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->bigInteger('height')->nullable()->change();
+            $table->string('work',50)->nullable()->change();
+            $table->text('comment')->nullable()->change();
+            $table->string('interest')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+        });
+    }
+}
